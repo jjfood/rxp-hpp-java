d47529a95a37e367a71006aae402e9d2ea396025
Fabio Falci <fabiofalci@gmail.com>
2026-06-18 15:15:11 +0100

    Fix validation

--- src/main/java/com/realexpayments/hpp/sdk/utils/ValidationUtils.java
+++ src/main/java/com/realexpayments/hpp/sdk/utils/ValidationUtils.java
@@ -9,6 +9,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
+import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,7 +39,10 @@ public class ValidationUtils {
 	 * Static initialiser instantiates validator object.
 	 */
 	static {
-		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		ValidatorFactory factory = Validation.byDefaultProvider()
+				.configure()
+				.messageInterpolator(new ParameterMessageInterpolator())
+				.buildValidatorFactory();
 		validator = factory.getValidator();
 	}
 
